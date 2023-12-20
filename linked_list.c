12d961a4287832eb9d32f60f07a7ba9a21dc0aa4
Mads Frandsen <madschristianfrandsen@gmail.com>
2023-12-20 20:56:30 +0100

    linked list

--- linked_list.c
+++ linked_list.c
@@ -9,12 +9,36 @@ Use a typedef struct to represent a node
 #include <stdlib.h>
 
 typedef struct {
-    int data;
+    int key;
     Node *next;
 } Node;
 
 
+Node *initNode(int key) {
+    Node *node = malloc(sizeof(Node));
+    node->key = key;
+    node->next = NULL;
+    return node;
+}
 
+void printList(Node *head) {
+    Node *temp = head;
+    while (temp != NULL) {
+        printf("%d\n", temp->key);
+        temp = temp->next;
+    }
+}
+
+int searchList(Node *head, int key) {
+    Node *temp = head;
+    while (temp != NULL) {
+        if (temp->key == key) {
+            return 1;
+        }
+        temp = temp->next;
+    }
+    return 0;
+}
 
 int main() {
     return 0;
