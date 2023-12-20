907f3ef3d9749c2344e5627d7ad988ef2b7bffa5
Mads Frandsen <madschristianfrandsen@gmail.com>
2023-12-20 20:30:14 +0100

    linked list

--- linked_list.c
+++ linked_list.c
@@ -1,9 +1,21 @@
 /*
 Linked list implementation:
 
-Use a typedef struct to represent
+Use a typedef struct to represent a node
 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct {
+    int data;
+    Node *next;
+} Node;
+
+
+
+
+int main() {
+    return 0;
+}
