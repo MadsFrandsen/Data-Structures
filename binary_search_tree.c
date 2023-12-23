d7d9ce06de32124a1a6941a5a8f122de5b999818
Mads Frandsen <madschristianfrandsen@gmail.com>
2023-12-23 09:40:36 +0100

    search added and printMenu

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -36,10 +36,58 @@ int insertNode(int data) {
 
 
 // remove
-// search
 
+// search
+int findNode(int data) {
+    Node *curr = root;
+
+    while (curr != NULL) {
+        if (curr->data == data) {
+            return 1;
+        } else {
+            if (curr->data > data) {
+                curr = curr->left;
+            } else {
+                curr = curr->right;
+            }
+        }
+    }
+    return 0;
+}
+
+
+void printMenu() {
+    printf("You can perform the following operations:\n");
+    printf("\t1. Add a node to the tree\n");
+    printf("\t2. Remove a node to the tree\n");
+    printf("\t3. Search for a given node\n");
+    printf("\t4. Quit\n");
+}
 
 
 int main() {
+
+
+    int operation = -1;
+    int num_received;
+
+    while (operation != 4) {
+        printMenu();
+        num_received = scanf("%d", &operation);
+        if (num_received == 1 && operation > 0 && operation <= 4) {
+            switch (operation) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    // Quit
+                    break;
+            }
+        }
+    }
+
     return 0;
 }
